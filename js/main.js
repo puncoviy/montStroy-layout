c252c5fe41d00531bfd0bb71bd93dabee5eb7ca6
puncoviy <usaspinner@gmail.com>
2022-07-25 14:46:17 +0300

    feat: finish partners section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.partners__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        autoplay: true
+    });
+});
