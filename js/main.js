a6c6ca0ea977d7caf17f23d20ef0d82bca237d56
puncoviy <usaspinner@gmail.com>
2022-08-09 14:23:23 +0300

    feat: add js for menu, add svg sprites

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+let scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 300
+});
+
 $(function(){
     $('.partners__slider').slick({
         slidesToShow: 4,
@@ -7,3 +11,18 @@ $(function(){
         autoplay: true
     });
 });
+
+const burgerBtn = document.querySelector('.burger-box');
+const burgerIcon = document.querySelector('.burger-menu');
+const menuList = document.querySelector('.header__menu-list');
+
+burgerBtn.addEventListener('click', function(){
+    burgerIcon.classList.toggle('active');
+    menuList.classList.toggle('active');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 100 && window.scrollY < 150) {
+            burgerIcon.classList.remove('active');
+            menuList.classList.remove('active');
+        }
+    })
+})
